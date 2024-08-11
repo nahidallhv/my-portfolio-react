f438da41bb965093d0a840ced4ec525a0dfbfb5e
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-08-11 14:52:49 +0400

    update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     const { name, email, message } = req.body;
 
     const transporter = nodemailer.createTransport({
-      service: 'Gmail', // Alternatif olarak SMTP sunucusu kullanabilirsiniz
+      service: 'Gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
