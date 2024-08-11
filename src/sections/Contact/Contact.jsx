dcdded813e71b5a3c2965f941dce3cfeaf54ec93
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-08-12 00:05:06 +0400

    update

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,10 +4,20 @@ import styles from './ContactStyles.module.css';
 
 function Contact() {
   const form = useRef();
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+
+    if (!userName || !userEmail || !message) {
+      setErrorMessage(alert("Please fill in all fields."));
+      return;
+    }
 
     emailjs
       .sendForm('service_szmpd4l', 'template_00d4828', form.current, {
@@ -16,9 +26,13 @@ function Contact() {
       .then(
         () => {
           console.log('SUCCESS!');
-          setSuccessMessage(alert('Your message has been sent successfully!'));
+          setSuccessMessage(alert("Your message has been sent successfully !"));
           form.current.reset();
-          setTimeout(() => setSuccessMessage(''), 2000); 
+          setUserName(''); 
+          setUserEmail('');
+          setMessage('');
+          setErrorMessage(''); 
+          setTimeout(() => setSuccessMessage(''), 5000); 
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -30,24 +44,45 @@ function Contact() {
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
       {successMessage && <div className="successMessage">{successMessage}</div>}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
       <form ref={form} onSubmit={sendEmail}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
           </label>
-          <input type="text" name="user_name" id="name" placeholder="Name" />
+          <input
+            type="text"
+            name="user_name"
+            id="name"
+            placeholder="Name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </div>
         <div className="formGroup">
           <label htmlFor="email" hidden>
             Email
           </label>
-          <input type="email" name="user_email" id="email" placeholder="Email" />
+          <input
+            type="email"
+            name="user_email"
+            id="email"
+            placeholder="Email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+          />
         </div>
         <div className="formGroup">
           <label htmlFor="message" hidden>
             Message
           </label>
-          <textarea name="message" id="message" placeholder="Message"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
         </div>
         <input className="hover btn" type="submit" value="Send" />
       </form>
